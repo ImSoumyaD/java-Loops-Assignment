a5c878e7646c0a95cdd9df2c7a73d3bba0876a15
Soumyadeep <149889474+ImSoumyaD@users.noreply.github.com>
2024-01-04 02:01:27 +0530

    question no - 2 solved
    Write a Java program to print a multiplication table for a given number 'n'.

---
+++ q2_multiplicationTable.java
@@ -0,0 +1,14 @@
+package PW.Loops_Assignment;
+
+import java.util.Scanner;
+
+public class q2_multiplicationTable {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        int n = sc.nextInt();
+        for( int i = 1; i<=10;i++){
+            System.out.println(n+" * "+i+" = "+n*i);
+        }
+    }
+}
